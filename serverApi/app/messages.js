dad7f4dcfbaa5b87a5263edbc35a2b77246ac6e2
SpeedrunDi <amanmaksat0@gmail.com>
2022-09-03 13:52:20 +0600

    Сделал запрос на messages sql

---
+++ serverApi/app/messages.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mysqlDb = require('../mysqlDb');
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  const news_id = req.query.news_id;
+
+  let messages;
+  try {
+    if (news_id) {
+      [messages] = await mysqlDb.getConnection().query(
+        'select ?, ?, ? from ?? where new_id = ?',
+        ['id', 'author', 'message', 'messages', news_id]
+        );
+    } else {
+      [messages] = await mysqlDb.getConnection().query('select id, author, message from messages');
+    }
+  } catch (e) {
+    console.log(e.message);
+  }
+  res.send(messages);
+});
+
+router.post('/', async (req, res) => {
+  if (!req.body.message && !req.query.news_id) {
+    return res.status(400).send({error: 'Data not valid'});
+  }
+
+  const messages = {
+    news_id: req.query.news_id,
+    author: req.body.author,
+    message: req.body.message
+  };
+
+  if (req.file) {
+    messages.image = req.file.filename;
+  }
+
+  let newNews;
+  try {
+    newNews = await mysqlDb.getConnection().query(
+      'insert into ?? (new_id, author, message) values (?, ?, ?)',
+      ['messages', messages.news_id, messages.author, messages.message]
+    );
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  res.send({
+    ...messages,
+    id: newNews[0].insertId
+  });
+});
+
+router.delete('/:id', async (req, res) => {
+  let response;
+  try {
+    response = await mysqlDb.getConnection().query(
+      'delete from ?? where id = ?',
+      ['messages', req.params.id]
+    );
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  if (response) {
+    res.send('Deletion completed!');
+  } else {
+    res.send('Deletion is not possible');
+  }
+});
+
+module.exports = router;
--- serverApi/index.js
+++ serverApi/index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const mysqlDb = require("./mysqlDb");
 const news = require('./app/news');
+const messages = require('./app/messages');
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 app.use(cors());
-app.use('/news', news)
+app.use('/news', news);
+app.use('/messages', messages);
 
 mysqlDb.connect();
 app.listen(port, () => {
